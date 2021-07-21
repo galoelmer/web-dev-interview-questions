084228d94a90afa438dd7769c2be179a61993a2b
Elmer C. Galo <elmer.galo@outlook.com>
2021-07-21 16:18:44 -0400

    chore: add eslint and prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import data from "./data";
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import data from './data'
 
 interface Quiz {
-  question: string;
-  answer: string | string[];
+  question: string
+  answer: string | string[]
 }
 
 function App() {
-  const [index, setIndex] = useState(0);
-  const [quiz, setQuiz] = useState<Quiz>(data[index]);
+  const [index, setIndex] = useState(0)
+  const [quiz, setQuiz] = useState<Quiz>(data[index])
 
   const prevOnClickHandler = () => {
     if (index === 0) {
-      setIndex(data.length - 1);
+      setIndex(data.length - 1)
     } else {
-      setIndex((state) => --state % data.length);
+      setIndex((state: number) => (state - 1) % data.length)
     }
-  };
+  }
 
   const nextOnClickHandler = () => {
     if (index === data.length - 1) {
-      setIndex(0);
+      setIndex(0)
     } else {
-      setIndex((state) => ++state % data.length);
+      setIndex((state) => (state + 1) % data.length)
     }
-  };
+  }
 
   useEffect(() => {
-    setQuiz(data[index]);
-  }, [index]);
+    setQuiz(data[index])
+  }, [index])
 
   return (
     <main>
@@ -37,8 +37,8 @@ function App() {
         <h3>{quiz.question}</h3>
         {Array.isArray(quiz.answer) ? (
           <ul>
-            {quiz.answer.map((item, i) => (
-              <li key={i}>{item}</li>
+            {quiz.answer.map((item) => (
+              <li key={item}>{item}</li>
             ))}
           </ul>
         ) : (
@@ -46,15 +46,27 @@ function App() {
         )}
       </section>
       <section className="buttons-container">
-        <div className="button" onClick={prevOnClickHandler}>
+        <div
+          className="button"
+          onClick={prevOnClickHandler}
+          onKeyUp={prevOnClickHandler}
+          role="button"
+          tabIndex={0}
+        >
           <span>&#10094;</span>
         </div>
-        <div className="button" onClick={nextOnClickHandler}>
+        <div
+          className="button"
+          onClick={nextOnClickHandler}
+          onKeyUp={nextOnClickHandler}
+          role="button"
+          tabIndex={0}
+        >
           &#10095;
         </div>
       </section>
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
